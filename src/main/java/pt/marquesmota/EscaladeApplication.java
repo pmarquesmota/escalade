7e90598da0558248016fce01e7efe9b46160f2e3
Paul Marques Mota <paul@marquesmota.pt>
2020-09-14 16:19:32 +0200

    ajout composant

--- src/main/java/pt/marquesmota/EscaladeApplication.java
+++ src/main/java/pt/marquesmota/EscaladeApplication.java
@@ -1,13 +1,29 @@
 package pt.marquesmota;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import pt.marquesmota.dao.TopoRepository;
+import pt.marquesmota.entity.Topo;
+
+@EnableAutoConfiguration
+@ComponentScan
 @SpringBootApplication
 public class EscaladeApplication {
-
+	
+	@Autowired
+	private TopoRepository topoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EscaladeApplication.class, args);
 	}
-
+	
+	public void run(String... args) throws Exception {
+		topoRepository.save( new Topo("test", "Topo de test", "chez moi", new Date(), true));
+	}
 }
--- src/main/java/pt/marquesmota/dao/TopoRepository.java
+++ src/main/java/pt/marquesmota/dao/TopoRepository.java
@@ -3,11 +3,13 @@ package pt.marquesmota.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import pt.marquesmota.entity.Topo;
 
+@Component
 public interface TopoRepository extends JpaRepository<Topo, Long>{
 	@Query(nativeQuery = true, value= "select * from topo t where t.nom=:x")
 	public Page<Topo> listTopos(@Param("x") String nom, Pageable pageable);
